36e1ebf95cac9963acd9c900561e77e8faea7d48
bloowper <bloowper@gmail.com>
2021-06-23 23:21:52 +0200

    .

--- src/main/java/orchowski/tomasz/ecommercedemo/command/ItemCommand.java
+++ src/main/java/orchowski/tomasz/ecommercedemo/command/ItemCommand.java
@@ -1,11 +1,8 @@
 package orchowski.tomasz.ecommercedemo.command;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.*;
 
+import lombok.*;
+import javax.validation.constraints.*;
 
 
 @Getter

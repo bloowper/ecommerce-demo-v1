5e2f6697badad51405f68e42ad13cf5fc3f6f6d2
bloowper <bloowper@gmail.com>
2021-06-24 20:23:30 +0200

    have changed version of hibernate-validator to older, new @valid works

---
+++ src/main/java/orchowski/tomasz/ecommercedemo/config/Validation.java
@@ -0,0 +1,15 @@
+package orchowski.tomasz.ecommercedemo.config;
+
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class Validation {
+//    @Bean
+//    public LocalValidatorFactoryBean validator() {
+//        return new LocalValidatorFactoryBean();
+//    }
+
+}

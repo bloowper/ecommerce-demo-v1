3127468427f63c2938ef20038a8ba7cc9883888d
tomek <tomasz.orchowski@stazysta.comarch.pl>
2021-07-26 09:04:58 +0200

    .

--- src/main/java/orchowski/tomasz/ecommercedemo/controller/customer/CartController.java
+++ src/main/java/orchowski/tomasz/ecommercedemo/controller/customer/CartController.java
@@ -3,12 +3,15 @@ package orchowski.tomasz.ecommercedemo.controller.customer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import orchowski.tomasz.ecommercedemo.command.ItemCommand;
 import orchowski.tomasz.ecommercedemo.converter.ItemToItemCommand;
 import orchowski.tomasz.ecommercedemo.domain.DeliveryAddress;
 import orchowski.tomasz.ecommercedemo.domain.Item;
+import orchowski.tomasz.ecommercedemo.domain.Order;
 import orchowski.tomasz.ecommercedemo.domain.User;
+import orchowski.tomasz.ecommercedemo.domain.enums.OrderState;
+import orchowski.tomasz.ecommercedemo.repository.DeliveryAddressRepository;
 import orchowski.tomasz.ecommercedemo.security.permision.isAuthenticated;
+import orchowski.tomasz.ecommercedemo.services.DeliveryAddressService;
 import orchowski.tomasz.ecommercedemo.services.ItemService;
 import orchowski.tomasz.ecommercedemo.services.UserService;
 import orchowski.tomasz.ecommercedemo.session.ShoppingCart;
@@ -23,6 +26,8 @@ import java.util.List;
 import java.util.UUID;
 
 
+
+
 @RequestMapping("user")
 @Controller
 @Slf4j
@@ -32,10 +37,15 @@ public class CartController {
 
     private final ItemService itemService;
     private final UserService userService;
+    private final DeliveryAddressService deliveryAddressService;
+
 
     private final ItemToItemCommand toItemCommand;
 
-
+    @ModelAttribute("order")
+    public Order setOrder() {
+        return new Order();
+    }
 
     @isAuthenticated
     @GetMapping("/cart")
@@ -86,15 +96,43 @@ public class CartController {
 
 
     @isAuthenticated
-    @PostMapping("buy/addressSelecting")
+    @PostMapping("/buy/addressSelecting")
     public String addressSelectingPost(Model model,
                                        Principal principal,
                                        HttpSession session,
-                                       @RequestParam Long id) {
+                                       @RequestParam Long id,
+                                       Order order) {
         //i guss that exist better solution for choosing one element from list by checkbox, but
         // i have no idea how to do that
         log.debug("chosen id: " + id);
-        return "redirect:/";
+        log.debug("order == null >>>"+(order==null));
+        DeliveryAddress address = deliveryAddressService.findById(id).orElseThrow(() -> new RuntimeException("Delivery address with given id not found!"));
+        ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+        if (order == null) {
+            throw new RuntimeException("Session attribute order not injected");
+        }
+        if (cart == null) {
+            throw new RuntimeException("Cart HttpSession attribute not found");
+        }
+        String userName = principal.getName();
+        User user = userService.findByUsername(userName).orElseThrow(() -> new RuntimeException(String.format("User %s not found", userName)));
+
+        order.setUser(user);
+        order.setOrderState(OrderState.PLACED);
+        order.setDeliveryAddress(address);
+
+
+        return "redirect:/user/buy/orderSummary";
+    }
+
+    @isAuthenticated
+    @GetMapping("/buy/orderSummary")
+    public String orderSummary(Model model,
+                               Principal principal,
+                               HttpSession session,
+                               Order order) {
+
+        return "user/orderSummary";
     }
 
 
